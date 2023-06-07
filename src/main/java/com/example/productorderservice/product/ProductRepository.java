72ad86491c514b9bf24d68dc6198e0c80bbba060
Dayeong <dayeongmaeng@gmail.com>
2023-06-07 23:09:23 +0900

    feat: JPA 적용하기

--- src/main/java/com/example/productorderservice/product/ProductRepository.java
+++ src/main/java/com/example/productorderservice/product/ProductRepository.java
@@ -1,17 +1,9 @@
 package com.example.productorderservice.product;
 
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-class ProductRepository {
+interface ProductRepository extends JpaRepository<Product, Long> {
 
-    private Map<Long, Product> persistence = new HashMap<>();
-    private Long sequence = 0L;
-
-    public void save(final Product product) {
-        product.assingId(++sequence);
-        persistence.put(product.getId(), product);
-    }
 }
