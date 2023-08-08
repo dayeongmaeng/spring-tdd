f8e701d705f34b8d5025ef99e6fab1cea79f9ba6
Dayeong <dayeongmaeng@gmail.com>
2023-08-08 22:48:54 +0900

    feat: POJO 주문 결제 기능 구현

---
+++ src/test/java/com/example/productorderservice/payment/Payment.java
@@ -0,0 +1,35 @@
+package com.example.productorderservice.payment;
+
+import com.example.productorderservice.order.Order;
+import org.springframework.util.Assert;
+
+class Payment {
+
+    private Long id;
+    private final Order order;
+
+    private final String cardNumber;
+
+    public Payment(final Order order, final String cardNumber) {
+        this.order = order;
+        this.cardNumber = cardNumber;
+        Assert.notNull(order, "주문은 필수입니다.");
+        Assert.notNull(cardNumber, "카드 번호는 필수입니다.");
+    }
+
+    public void assignId(final Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public int getPrice() {
+        return order.getTotalPrice();
+    }
+}
